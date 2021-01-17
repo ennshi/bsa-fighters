3256cea0d9d14e6096cdc23ceafe5712c4d10f0b
Anna Sh <47815524+ennshi@users.noreply.github.com>
2021-01-18 00:00:15 +0100

    Refactor FightersSelectorView

--- src/javascript/components/FightersSelectorView.js
+++ src/javascript/components/FightersSelectorView.js
@@ -4,19 +4,8 @@ import SelectedFighterView from './SelectedFighterView';
 class FightersSelectorView extends View {
     constructor() {
         super();
-        this.selector = new Map();
-        this.nextFighter = 'left';
-        this.setFighterCb = this.setFighter.bind(this);
-    }
-
-    setFighter(fighter) {
-        this.selector.set(this.nextFighter, fighter);
-        this.nextFighter = this.nextFighter === 'left' ? 'right' : 'left';
-        this.displaySelectedFighters();
-    }
-
-    isFull() {
-        return (this.selector.size === 1);
+        this.createFightersSelector();
+        this.updateFightersSelectorView = this.displaySelectedFighters.bind(this);
     }
 
     createFightersSelector() {
@@ -24,17 +13,17 @@ class FightersSelectorView extends View {
         return this.element;
     }
 
-    displaySelectedFighters() {
-        if(this.selector.size) {
-            const fighters = Array.from(this.selector.entries()).map(fighter => {
+    displaySelectedFighters(fighters) {
+        if(fighters) {
+            const fighterElements = fighters.map(fighter => {
                 const fighterElement = new SelectedFighterView(fighter[1], fighter[0]);
                 return fighterElement.element;
             });
             this.element.innerHTML = '';
-            this.element.append(...fighters);
+            this.element.append(...fighterElements);
             return this.element;
         }
     }
 }
 
-export const fightersSelectorView = new FightersSelectorView();
+export default FightersSelectorView;
