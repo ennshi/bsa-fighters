09d26bd6f4452a177254b985d546a100cd5427ba
Anna Sh <47815524+ennshi@users.noreply.github.com>
2021-01-17 20:47:59 +0100

    Create FightersSelectorView

---
+++ src/javascript/components/FightersSelectorView.js
@@ -0,0 +1,41 @@
+import View from './View';
+import FighterView from './FighterView';
+
+class FightersSelectorView extends View {
+    constructor() {
+        super();
+        this.selector = new Map();
+        this.nextFighter = 'left';
+        this.setFighterCb = this.setFighter.bind(this);
+    }
+
+    setFighter(fighter) {
+        this.selector.set(this.nextFighter, fighter);
+        this.nextFighter = this.nextFighter === 'left' ? 'right' : 'left';
+        this.displaySelectedFighters();
+    }
+
+    isFull() {
+        return (this.selector.size === 1);
+    }
+
+    createFightersSelector() {
+        this.element = this.createElement({tagName: 'div', className: 'fighters-selector__container', attributes: {id: 'arena'}});
+        return this.element;
+    }
+
+    displaySelectedFighters() {
+        if(this.selector.size) {
+            const fighters = Array.from(this.selector.entries()).map(fighter => {
+                const fighterElement = new FighterView(fighter[1], null);
+                fighterElement.element.classList.add(fighter[0]);
+                return fighterElement.element;
+            });
+            this.element.innerHTML = '';
+            this.element.append(...fighters);
+            return this.element;
+        }
+    }
+}
+
+export const fightersSelectorView = new FightersSelectorView();
